39a1274db87ffaf8fb82ec61fd446c61ac20a2cd
xyyx <xyyx@mail.ru>
2017-12-16 22:07:37 +0800

    Import Lineage doze from op3 tree

--- device.mk
+++ device.mk
@@ -385,7 +385,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    XiaomiDoze
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
