3fabe35fa08c9274c8cdacb8c7f5e62ce77e68be
Ravi Pingat <pingatravi@gmail.com>
2018-01-15 16:42:06 +0530

    Update cryptfs_hw
    Squash of the following commits:
    
    cryptfs_hw: Remove android reboot during password check
    
    After reaching the maximum number of retires with wrong password,
    userdata is erased based on the info written on /cache partition.
    As per the latest changes of A/B, cache partition is not present
    which leads to failure of userdata wipe before reboot. This patch
    removes reboot from cryptfs_hw layer if max entries reached, as
    frameworks take care of userdata wipe.
    
    Change-Id: If97f2f7452beb004f44d7d41d1d76def23fe2720
    
    cryptfs_hw: Add new APIs for key management
    
    Add new APIs for create, wipe and update keys, which is
    required for HW based Full Disk Encryption.
    
    Change-Id: I483ce1a734db7b7cbfb2a06fe74baf559cfb51fb
    
    cryptfs_hw: Fix compilation error
    
    Fix compilation error for qseecom.h which encountered while
    creating the clean build.
    
    Change-Id: I1271866d453f0f3d60bfc23ac12819b1cd0cabea

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -12,7 +12,10 @@ commonSharedLibraries := \
                         libhardware \
                         liblog
 commonIncludes := \
-                  hardware/libhardware/include/hardware/
+                  hardware/libhardware/include/hardware/ \
+                  $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_C_INCLUDES := $(commonIncludes)
 LOCAL_SRC_FILES := $(sourceFiles)
